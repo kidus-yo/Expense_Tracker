82e0a464c2df63d482b47bc00f9d3a49618fa1a6
kidus-yo <kidusyonas96@gmail.com>
2026-08-18 15:33:01 +0300

    Error

--- filewrite.py
+++ filewrite.py
@@ -3,9 +3,19 @@ import json
 import csv
 
 def save_files(expenses):
-    file_path = "C:/Users/victus/OneDrive/Desktop/expenses.txt"
+    file_path = "C:/Users/victus/OneDrive/Desktop/expenses.json"
 
-    with open(file_path, "w") as file:
-     for lists in expenses:
-      file.write(lists)
+    data = []
+
+    data.append({
+  
+      "description": expenses.description,
+      "catagory": expenses.catagory,
+      "amount": expenses.amount,
+      "date": expenses.date.strftime("%Y-%m-%d"),
+
+    })
+
+    with open(file_path, "x") as file:
+     json.dump(data, file, indent=4)
      print(f"Json file Created with path {file_path}")
