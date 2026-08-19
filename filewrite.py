ececa05762ae8c943e4f69bdddf7b9bcd024eb05
kidus-yo <kidusyonas96@gmail.com>
2026-08-19 13:06:22 +0300

    updated

--- filewrite.py
+++ filewrite.py
@@ -6,16 +6,16 @@ def save_files(expenses):
     file_path = "C:/Users/victus/OneDrive/Desktop/expenses.json"
 
     data = []
-
-    data.append({
-  
-      "description": expenses.description,
-      "catagory": expenses.catagory,
-      "amount": expenses.amount,
-      "date": expenses.date.strftime("%Y-%m-%d"),
-
+    for expense in expenses:
+     data.append({
+    
+      "description": expense.description,
+      "catagory": expense.catagory,
+      "amount": expense.amount,
+      "date": expense.date.strftime("%Y-%m-%d"),
+                                                                                                                            
     })
 
-    with open(file_path, "x") as file:
+    with open(file_path, "w") as file:
      json.dump(data, file, indent=4)
      print(f"Json file Created with path {file_path}")
